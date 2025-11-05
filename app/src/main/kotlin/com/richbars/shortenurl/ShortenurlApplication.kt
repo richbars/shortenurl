f1991c01d27411428db8314547c2ff63fb503b2e
richa <richard.santos21@hotmail.com>
2025-11-05 18:28:07 -0300

    init develop

---
+++ app/src/main/kotlin/com/richbars/shortenurl/ShortenurlApplication.kt
@@ -0,0 +1,11 @@
+package com.richbars.shortenurl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShortenurlApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShortenurlApplication>(*args)
+}
